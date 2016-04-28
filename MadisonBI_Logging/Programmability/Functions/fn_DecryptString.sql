7dba0dc58543e2590c0252306a87db4ea9df3442
quietseason <hello000163@163.com>
2016-04-28 18:00:35 +0800

    Adjust previous work and complete most of the first staging package

--- MadisonBI_Logging/Programmability/Functions/fn_DecryptString.sql
+++ MadisonBI_Logging/Programmability/Functions/fn_DecryptString.sql
@@ -1,9 +1,17 @@
 ﻿CREATE FUNCTION [dbo].[fn_DecryptString]
 (
-	@Ciphertext VARCHAR(1000)
+	@Ciphertext VARCHAR(1200)
 )
 RETURNS VARCHAR(500)
 AS
 BEGIN
-	RETURN @param1 + @param2
+	DECLARE @PosCurr INT = 2, @MaxLen INT, @Plaintext VARCHAR(500) = '', @Code INT
+	SELECT @MaxLen = LEN(@Ciphertext)
+	WHILE @PosCurr < @MaxLen
+	BEGIN
+		SET @Code = CONVERT(INT,CONVERT(VARBINARY(3),'0x' + SUBSTRING(@Ciphertext,@PosCurr,2),1))
+		SET @Plaintext = @Plaintext + CHAR(@Code)
+		SET @PosCurr = @PosCurr + 2
+	END
+	RETURN @Plaintext
 END
