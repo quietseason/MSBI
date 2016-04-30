d877b10a1942a0f03905d5e6580342132960aa64
quietseason <hello000163@163.com>
2016-04-30 08:27:50 +0800

    Completed the first staging package and starting to design archiving logic

--- MadisonBI_Archive/Tables/ARCH_accountinfo.sql
+++ MadisonBI_Archive/Tables/ARCH_accountinfo.sql
@@ -2,21 +2,23 @@
 (
 	[ArchiveID] BIGINT NOT NULL IDENTITY(1,1),
 	[StagingID] BIGINT NOT NULL,
-	[AccountID] BIGINT NOT NULL, 
-    [Name] NVARCHAR(200) NULL, 
+	[AccountID] BIGINT NOT NULL, --账户ID, Source Key
+    [Name] NVARCHAR(200) NULL, --用户名
     [Mobile] NVARCHAR(50) NULL, 
-    [Age] INT NULL, 
-    [Gender] VARCHAR(20) NULL, 
-    [ReferralCode] VARCHAR(20) NULL, 
-    [InvitedBy] VARCHAR(20) NULL, 
-    [Channel] VARCHAR(100) NULL, 
-    [IsPWDSet] BIT NULL, 
-    [IsRealName] BIT NULL, 
-    [IsCardBinded] BIT NULL, 
+    [IDType] TINYINT NULL, --证件类型 0：身份证
+    [IDNumber] NVARCHAR(100) NULL, --证件号
+    [ReferralCode] NVARCHAR(20) NULL, --推荐代码
+    [InvitedBy] NVARCHAR(20) NULL, --推荐人代码
+    [Status] NVARCHAR(100) NULL, --账户状态 0：未激活 1：激活
+	[CustomStatus] TINYINT NULL, --自定义状态 1：密码已设置 2：已实名认证(包含1) 3：银行卡已绑定(包含1和2)
     [Created] DATETIME NOT NULL, 
     [Modified] DATETIME NULL, 
-    [Source] VARCHAR(100) NOT NULL, 
+    [Source] NVARCHAR(100) NOT NULL, 
     [StagingInsertTime] DATETIME NOT NULL, 
 	[ArchiveInsertTime] DATETIME NOT NULL, 
     CONSTRAINT [PK_ARCH_accountinfo] PRIMARY KEY(ArchiveID)
 )
+/*
+Create a partition for every 2 months data(Pre-defined partition for next 2 months)
+Truncate the partition for 6 months ago and remove the partition before that
+*/
