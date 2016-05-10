d1ec40377eb4b5865f93678d2d143c3e7525c8db
quietseason <hello000163@163.com>
2016-05-10 18:03:09 +0800

    Fixed bugs for previous work

---
+++ MadisonBI_DataWarehouse/Programmability/Stored Procedures/usp_Merge_DeletedAccount.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[usp_Merge_DeletedAccount]
+	@InsertedRows BIGINT,
+	@UpdatedRows BIGINT
+AS
+BEGIN
+	SET @UpdatedRows = -1
+	INSERT INTO DBO.DeletedAccount
+	SELECT 
+		A.AccountID AS DeletedAccountID,
+		GETDATE() AS ETLInsertTime
+	FROM DBO.AccountInfo A WITH(NOLOCK)
+	WHERE A.IsCurrent = 1
+	AND NOT EXISTS(
+		SELECT 1 FROM DBO.CLEAN_DeletedAccount CD WITH(NOLOCK)
+		WHERE A.AccountID = CD.DeletedAccountID
+	)
+	AND NOT EXISTS(
+		SELECT 1 FROM DBO.DeletedAccount D WITH(NOLOCK)
+		WHERE A.AccountID = D.DeletedAccountID
+	)
+	SET @InsertedRows = @@ROWCOUNT
+END
---
+++ MadisonBI_DataWarehouse/Tables/DeletedAccount.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[DeletedAccount]
+(
+	[DeletedAccountID] BIGINT NOT NULL,--被删除的账户ID
+	[ETLInsertTime] DATETIME NOT NULL, 
+	CONSTRAINT [PK_DeletedAccount] PRIMARY KEY([DeletedAccountID])
+)
